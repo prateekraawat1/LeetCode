a0cc08fecc0f326d6a5d56cbe436b24e14523327
Prateek Rawat <prateekj297@Gmail.com>
2022-07-26 16:06:12 +0530

    Create RemoveDuplicatesFromSortedArray.py

---
+++ RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 1
+        while (i < len(nums)):
+            if nums[i] == nums[i -1]:
+                nums.pop(i)
+            else:
+                i = i + 1
+                
+        return len(nums)
